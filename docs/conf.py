cd7e7ffd43b657a6687e23942eba7a1862a9a47f
cwise24 <cwise24@gmail.com>
2019-07-02 02:14:34 -0700

    buttons

--- docs/conf.py
+++ docs/conf.py
@@ -63,5 +63,17 @@ html_context = {
 }
 
 html_theme_options = {
-        'prev_next_buttons_location': 'bottom'
-        }
+    'canonical_url': '',
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
