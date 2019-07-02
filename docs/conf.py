a1214534f70c32f5fe75c09328d943dd2648c3bb
cwise24 <cwise24@gmail.com>
2019-07-02 02:59:20 -0700

    commented out html options

--- docs/conf.py
+++ docs/conf.py
@@ -62,18 +62,18 @@ html_context = {
 "commit": False,
 }
 
-html_theme_options = {
+#html_theme_options = {
 #    'canonical_url': '',
 #    'logo_only': False,
 #    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
+#    'prev_next_buttons_location': 'bottom',
 #    'style_external_links': False,
 #    'vcs_pageview_mode': '',
 #    'style_nav_header_background': 'white',
     # Toc options
 #    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
+#    'sticky_navigation': True,
+#    'navigation_depth': 4,
 #    'includehidden': True,
 #    'titles_only': False
-}
+#}
